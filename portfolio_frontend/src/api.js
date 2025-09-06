bb86223a4aa09e62955323f442756b4b06d8f947
Pravin <pravinfrancis241@gmail.com>
2025-09-06 21:37:22 +0530

    First stage of deploying frontend

--- portfolio_frontend/src/api.js
+++ portfolio_frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portfolio-1-t4b1.onrender.com/api/", // Django API runs here
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
